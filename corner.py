7132063c60bc5b12ff10f41e9c72d24fe5b305aa
Jimmy Vlekke <jimmyvlekke@gmail.com>
2021-03-25 12:06:03 +0100

    Wizards of corners!

--- corner.py
+++ corner.py
@@ -1,7 +1,9 @@
 import os
+import sys
+
 import torch
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 from tqdm import tqdm
 from numpy import asarray
 
@@ -37,6 +39,67 @@ def get_images(path):
         for i in range(4):
             images.append(np.rot90(image, i).flatten())
     return images
+
+
+def draw_shapes(draw):
+    shape_coordinates = [[(4, 4), (20, 20)], [(30, 4), (50, 20)], [(4, 30), (20, 50)], [(30, 30), (50, 50)]]
+    corner_coordinates = []
+
+    for coordinate in shape_coordinates:
+        corner_coordinates.append(draw_rectangle(draw, coordinate[0], coordinate[1]))
+
+    return [item for sublist in corner_coordinates for item in sublist]
+
+
+def draw_rectangle(draw, top_left, bottom_right):
+    draw.rectangle((top_left, bottom_right), outline="white")
+    top_right = (bottom_right[0], top_left[1])
+    bottom_left = (top_left[0], bottom_right[1])
+    return top_left, top_right, bottom_left, bottom_right
+
+
+def draw_line(draw, left, right):
+    print()
+
+
+def draw_ellipse(draw, left, right):
+    print()
+
+
+def draw_polygon(draw, left, right):
+    print()
+
+
+def wizard():
+    img_size = 64
+    img = Image.new("L", (img_size, img_size))
+    draw = ImageDraw.Draw(img)
+    corners = draw_shapes(draw)
+    img.save("test.png")
+    sliding_window = 8
+
+    corner_images = []
+    no_corner_images = []
+
+    for i in range(img_size - sliding_window):
+        for j in range(img_size - sliding_window):
+            sub_img = img.crop((i, j, i + sliding_window, j + sliding_window))
+            is_corner = False
+            for corner in corners:
+                if i + (sliding_window / 4) <= corner[0] < i + sliding_window - (sliding_window / 4) and +\
+                        j + (sliding_window / 4) <= corner[1] < j + sliding_window - (sliding_window / 4):
+                    is_corner = True
+                    break
+
+            if is_corner:
+                corner_images.append(sub_img)
+            else:
+                no_corner_images.append(sub_img)
+
+    corner_images[11].save("corner.png")
+    no_corner_images[11].save("no_corner.png")
+    print(len(corner_images))
+    print(len(no_corner_images))
 
 
 def main():
@@ -93,4 +156,5 @@ def main():
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main()
+    # main()
+    wizard()
