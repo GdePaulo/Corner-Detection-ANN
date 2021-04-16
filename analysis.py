e5f7aae5a646d3de3c7e9e21cbc70fefefe352e5
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2021-04-16 14:55:44 +0200

    Fix data loading

--- analysis.py
+++ analysis.py
@@ -1,9 +1,14 @@
+import os
+from os import path
+import pickle
 
 def runAnalysis():
     k_data = [[] for x in range(4)]
     if  path.exists("k_data.pickle"):
         k_data = pickle.load(open("k_data.pickle", "rb"))
     training_losses, testing_losses, training_accuracies, testing_accuracies = k_data
+    
+    print(f"Loading data from {len(k_data)} runs")
         
 if __name__ == "__main__":
     runAnalysis()
